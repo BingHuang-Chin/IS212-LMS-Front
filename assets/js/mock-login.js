6dbca516566d37d8c0e80c96a8b57dc5dcfac49b
Bing Huang <bing2609@gmail.com>
2021-09-25 00:30:24 +0800

    assets: js: mock-login: added login credentials for hr, trainer and learners to mock their account
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- assets/js/mock-login.js
+++ assets/js/mock-login.js
@@ -1,11 +1,31 @@
-$(document).ready(() => {
+/**
+ * Username and passwords are left exposed intentionally so that we are able
+ * to mock the information prior to Azure SSO implementation
+ */
+const loginAsHr = () => {
   auth0App.client.login({
     realm: "Username-Password-Authentication",
     username: "hr@aio.com",
-    password: "thisisahruser"
+    password: "Pass123$"
   }, onLoginSuccessful)
-})
+}
+
+const loginAsTrainer = () => {
+  auth0App.client.login({
+    realm: "Username-Password-Authentication",
+    username: "trainer@aio.com",
+    password: "Pass123$"
+  }, onLoginSuccessful)
+}
+
+const loginAsLearner = () => {
+  auth0App.client.login({
+    realm: "Username-Password-Authentication",
+    username: "learner@aio.com",
+    password: "Pass123$"
+  }, onLoginSuccessful)
+}
 
 const onLoginSuccessful = (err, result) => {
-  console.log(err, result)
+  console.log(result.idToken)
 }
