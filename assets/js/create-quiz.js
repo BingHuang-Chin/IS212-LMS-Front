a0b6ff13ace33137d6fba0b859e9a26a531085b6
Bing Huang <bing2609@gmail.com>
2021-09-25 23:05:02 +0800

    js: create-quiz: fixed radio button not in single select mode
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- assets/js/create-quiz.js
+++ assets/js/create-quiz.js
@@ -11,16 +11,23 @@ const generateQuestionCard = () => {
   $(radioButtonElement).attr("name", `question-${totalQuestions}`)
   $(radioButtonElement).attr("checked", true)
 
+  // Setup event listener for adding new options
+  const addOptionButtonElement = $(questionElement).find(".add-option")[0]
+  $(addOptionButtonElement).attr("onclick", `generateOption(this, ${totalQuestions})`)
+
   $("#question-body").append(questionElement)
 }
 
-const generateOption = element => {
+const generateOption = (element, questionNumber) => {
   const parents = $(element).parents()
   const cardElement = parents[2]
   const questionTypeElement = $(cardElement).find(".section-id")[0]
   const questionType = $(questionTypeElement).val()
-  
-  const optionInputElement = $("#option-input-template").html()
+
+  const optionInputElement = $($("#option-input-template").html())
+  const radioButtonElement = $(optionInputElement).find(".form-check-input")[0]
+  $(radioButtonElement).attr("name", `question-${questionNumber}`)
+
   const optionsElement = $(cardElement).find(".options")[0]
   $(optionsElement).append(optionInputElement)
 }
