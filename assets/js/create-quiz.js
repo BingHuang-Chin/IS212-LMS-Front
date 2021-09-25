a5fab99579b4819c7ca41f66da8ddf18679c6ea4
Bing Huang <bing2609@gmail.com>
2021-09-25 21:42:13 +0800

    js: create-quiz: generateOption: initial implementation Signed-off-by: Bing Huang <bing2609@gmail.com>

--- assets/js/create-quiz.js
+++ assets/js/create-quiz.js
@@ -2,3 +2,11 @@ const generateQuestionCard = () => {
   const questionTemplate = $("#question-template").html()
   $("#question-body").append(questionTemplate)
 }
+
+const generateOption = element => {
+  console.log(element)
+}
+
+$(document).ready(() => {
+  generateQuestionCard()
+})
