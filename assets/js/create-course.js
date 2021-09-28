0b4c6a5faaa38bd9360d7544119d6076e191f9d9
Josh <josh-koh@hotmail.com>
2021-09-28 18:49:11 +0800

    JS & HTML: repositioned script tag for authentication in html and onload for js

--- assets/js/create-course.js
+++ assets/js/create-course.js
@@ -2,30 +2,6 @@
 Fetch the values of the form submit and send it to hasura database using graphQL
 */
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict';
-    $('#header').load("/common/navbar.html");
-
-
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation');
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms).forEach((form) => {
-        form.addEventListener('submit', (event) => {
-            event.preventDefault()
-            if (!form.checkValidity()) {
-                event.stopPropagation();
-            }
-            else {
-                createCourse()
-            }
-            form.classList.add('was-validated');
-        }, false);
-    });
-})();
-
-
-
 const GRAPHQL_ENDPOINT = "http://localhost:8080/v1/graphql"
 
 
@@ -89,24 +65,60 @@ async function createCourse() {
 }
 
 
+async function getDropdownOptions() {
+    const response = await fetch(GRAPHQL_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': getIdToken(),
+        },
+        body: JSON.stringify({
+            query: `
+                query {
+                    badge {
+                    id
+                    title
+                    }
+                }
+            `
+        })
+    })
+    const { errors, data } = await response.json()
+    if (errors) {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Failed to retrieve drop down',
+            icon: 'error'
+        }).then(result => {
+            if (result.isDismissed || result.isConfirmed)
+                location.reload()
+        })
+        return
+    }
+
+    for (const option in data.badge) {
+        $ = `<option value="${option.id}>${option.title}</option>`
+        }
+}
 
 
-// async function getDropdownOptions() {
-//     const response = await fetch(GRAPHQL_ENDPOINT, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             authorization: getIdToken(),
-//         },
-//         body: JSON.stringify({
-//             query: `
-//                 query {
-//                     badge_id
-//                 }
-//             `
-//         })
-//     })
-
-//     const responseJson = await response.json()
-//     return responseJson
-// }
+(() => {
+    'use strict';
+    $('#header').load("/common/navbar.html");
+    getDropdownOptions()
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation');
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms).forEach((form) => {
+        form.addEventListener('submit', (event) => {
+            event.preventDefault()
+            if (!form.checkValidity()) {
+                event.stopPropagation();
+            }
+            else {
+                createCourse()
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();
