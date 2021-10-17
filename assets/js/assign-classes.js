01368c0429aae57f068a2097f4841ba60c5fd15b
Josh <josh-koh@hotmail.com>
2021-10-17 22:12:57 +0800

    js: assign-classes: dropdown list for learners name

--- assets/js/assign-classes.js
+++ assets/js/assign-classes.js
@@ -35,11 +35,18 @@ async function getLearners() {
     }
 
     for (const learner of data.learner) {
-        console.log(learner.name)
+        list_learners = `
+            <a class="dropdown-item" value="${learner.name}">${learner.name}</a>
+        `
+        $("#dropdown1").append(list_learners)
     }
 }
 
 getLearners()
+
+
+
+
 
 async function getClasses() {
     const params = new URLSearchParams(window.location.search)
@@ -95,13 +102,18 @@ async function getClasses() {
                 <div class="card ms-3 me-3 mt-3 mb-3 col-md-4">
                     <div class="card-body">
                         <h5 class="card-title mb-3"><strong><u>${classes.name}</u></strong></h5>
-                        <p class="card-text"><strong>Class Size: ${classes.class_size}</strong>
-                        <p class="card-text"><strong>Course start date: ${classes.start_date}</strong>
-                        <p class="card-text"><strong>Course end date: ${classes.end_date}</strong>
-                        <p class="card-text"><strong>Class start time: ${classes.class_start_time}</strong>
-                        <p class="card-text"><strong>Class end time: ${classes.class_end_time}</strong>
-                        <p class="card-text"><strong>Trainer: ${getTrainerName(classes.trainer)}</strong>
-                        <>
+                        <p class="card-text"><strong>Class Size: ${classes.class_size}</strong></p>
+                        <p class="card-text"><strong>Course start date: ${classes.start_date}</strong></p>
+                        <p class="card-text"><strong>Course end date: ${classes.end_date}</strong></p>
+                        <p class="card-text"><strong>Class start time: ${classes.class_start_time}</strong></p>
+                        <p class="card-text"><strong>Class end time: ${classes.class_end_time}</strong></p>
+                        <p class="card-text"><strong>Trainer: ${getTrainerName(classes.trainer)}</strong></p>
+                        <div class="dropdown">
+                            <button type="button" class="btn btn-secondary dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
+                                View learners
+                            </button>
+                            <div class="dropdown-menu" id="dropdown1" aria-labelledby="dropdownMenuOffset"></div>
+                        </div>
                     </div>
                 </div>`
             $("#cardColumns").append(cards)
