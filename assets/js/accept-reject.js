34a9246fae693c30cc9e841e9f00fad482d38fab
yanggg <nigelyeo1996@gmail.com>
2021-10-19 19:19:46 +0800

    added the course links

--- assets/js/accept-reject.js
+++ assets/js/accept-reject.js
@@ -3,7 +3,7 @@ Fetch the values of the form submit and send it to hasura database using graphQL
 */
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 const GRAPHQL_ENDPOINT = "http://localhost:8080/v1/graphql"
-$('#header').load("/common/navbar.html");
+// $('#header').load("/common/navbar.html");
 
 async function getcourses(){
     const [title, description, start_date, end_date] = [
@@ -38,21 +38,27 @@ async function getcourses(){
         })
         
     })
-    
+
     const dataset = await response.json()
     console.log(dataset.data.course[0])
     for(const courses of dataset.data.course){
         cards = `
-        <div class="col-sm-4" id="pre"> 
-            <div class="card" style="width:400px">
-                <img class="card-img-top" src="/assets/images/jennie.jpeg" alt="Card image">
-                <div class="card-body">
-                <h4 class="card-title">${courses.title}</h4>
-                <p class="card-text">${courses.description}</p>
-                <a href="#" class="btn btn-primary">View Course</a>
-            </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+        <!-- Card-->
+        <div class="card rounded shadow-sm border-0">
+        <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+            <h5> <a href="http://localhost:3000/pages/course_content" class="text-dark">${courses.title}</a></h5>
+            <p class="small text-muted font-italic">${courses.description}</p>
+            <ul class="list-inline small">
+            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+            <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+            </ul>
         </div>
-        
+        </div>
+        </div>
         `
         document.getElementById('cardcolumns').innerHTML+= cards
 
@@ -61,3 +67,5 @@ async function getcourses(){
 }
 
 getcourses()
+
+
---
+++ assets/js/course_content.js
@@ -0,0 +1,16 @@
+const GRAPHQL_ENDPOINT = "http://localhost:8080/v1/graphql"
+$('#header').load("/common/navbar.html");
+
+
+
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
