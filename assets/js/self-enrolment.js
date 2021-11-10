dee4afa5fbd0e279ae05404ffa83636d1665b1e8
Bing Huang <bing2609@gmail.com>
2021-11-11 03:30:02 +0800

    js: self-enrolment: added ui and logic to display class list
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- assets/js/self-enrolment.js
+++ assets/js/self-enrolment.js
@@ -66,8 +66,44 @@ function renderCourseList (courses) {
 }
 
 async function onEnrolCourse (courseId) {
-  const learnerId = FAKE_LEARNER_ID // TODO: Replace with actual learner id
+  const [classes, vacancy] = await getAvailableClasses(courseId)
+  let classElement = ''
 
+  for (const lesson of classes) {
+    const { id, class_size, name, class_start_time, class_end_time } = lesson
+    if (vacancy[id] !== undefined && vacancy[id] >= class_size)
+      continue
+
+    const actualClassSize = vacancy[id] === undefined
+      ? class_size
+      : class_size - vacancy[id]
+
+    classElement += `
+      <div class="col card mb-2">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p>Space left: ${actualClassSize}</p>
+        </div>
+      </div>
+    `
+  }
+
+  if (classElement.length === 0)
+    classElement = "<p>No classes available.</p>"
+
+  Swal.fire({
+    title: "Select a class to enrol",
+    showConfirmButton: false,
+    showCancelButton: true,
+    html: `
+      <div class="row row-cols-1 m-0 p-0">
+        ${classElement}
+      </div>
+    `
+  })
+}
+
+async function getAvailableClasses (courseId) {
   const response = await fetch(GRAPHQL_ENDPOINT, {
     method: "POST",
     headers: {
@@ -76,13 +112,22 @@ async function onEnrolCourse (courseId) {
     },
     body: JSON.stringify({
       query: `
-        mutation {
-          enrolCourse(object: {courseId: ${courseId}, learnerId: ${learnerId}}) {
-            status
-            message
+        query {
+          course_by_pk(id: ${courseId}) {
+            classes {
+              id
+              name
+              class_size
+              class_start_time
+              class_end_time
+            }
+            
+            enrolments(where: { status_id: { _eq: 2 } }) {
+              class_id
+            }
           }
         }
-      `
+        `
     })
   })
 
@@ -94,17 +139,59 @@ async function onEnrolCourse (courseId) {
       icon: 'error'
     })
 
-  const { status, message } = data
-  if (status !== 200)
-    return Swal.fire({
-      title: 'Error!',
-      text: message,
-      icon: 'error'
-    })
+  const { classes, enrolments } = data.course_by_pk
+  const enrolmentDict = {}
 
-  Swal.fire({
-    title: 'Success!',
-    text: message,
-    icon: 'success'
+  enrolments.forEach(({ class_id }) => {
+    if (enrolmentDict[class_id] === undefined)
+      enrolmentDict[class_id] = 1
+    else
+      enrolmentDict[class_id] += 1
   })
+
+  return [classes, enrolmentDict]
+}
+
+async function onClassSelected (courseId, classId) {
+  // const learnerId = FAKE_LEARNER_ID // TODO: Replace with actual learner id
+
+  // const response = await fetch(GRAPHQL_ENDPOINT, {
+  //   method: "POST",
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'authorization': getIdToken(),
+  //   },
+  //   body: JSON.stringify({
+  //     query: `
+  //       mutation {
+  //         enrolCourse(object: {courseId: ${courseId}, learnerId: ${learnerId}}) {
+  //           status
+  //           message
+  //         }
+  //       }
+  //     `
+  //   })
+  // })
+
+  // const { errors, data } = await response.json()
+  // if (errors)
+  //   return Swal.fire({
+  //     title: 'Error!',
+  //     text: 'Something went wrong.',
+  //     icon: 'error'
+  //   })
+
+  // const { status, message } = data
+  // if (status !== 200)
+  //   return Swal.fire({
+  //     title: 'Error!',
+  //     text: message,
+  //     icon: 'error'
+  //   })
+
+  // Swal.fire({
+  //   title: 'Success!',
+  //   text: message,
+  //   icon: 'success'
+  // })
 }
