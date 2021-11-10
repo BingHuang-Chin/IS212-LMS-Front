f3711f8385df21933bee33564ab4aed149672584
Bing Huang <bing2609@gmail.com>
2021-11-10 23:48:13 +0800

    js: trainer-home: added retrieval and rendering of quizzes owned by trainer id of 1
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- assets/js/trainer-home.js
+++ assets/js/trainer-home.js
@@ -1,3 +1,48 @@
-$(document).ready(function () {
+const GRAPHQL_ENDPOINT = "http://localhost:8080/v1/graphql"
+const FAKE_TRAINER_ID = 1
+
+$(document).ready(async function () {
   $("#navbar").load("/common/navbar.html")
+
+  const quizzes = await getQuizzes(FAKE_TRAINER_ID)
+  quizzes.forEach(({ id, title, time_limit }) => {
+    $("#quiz-list").first().append(`
+      <div class="card mb-4">
+        <div class="d-flex card-body justify-content-between align-items-center">
+          <p class="mb-0">${title}</p>
+
+          <div>
+            <button class="btn text-danger">Delete</button>
+            <button class="btn">Edit</button>
+          </div>
+        </div>
+      </div>
+    `)
+  })
 })
+
+async function getQuizzes (trainerId) {
+  const response = await fetch(GRAPHQL_ENDPOINT, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'authorization': getIdToken(),
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          section(where: {trainer_id: {_eq: ${trainerId}}}) {
+            quizzes {
+              id
+              title
+              time_limit
+            }
+          }
+        }      
+      `
+    })
+  })
+
+  const responseJson = await response.json()
+  return responseJson.data.section.map(section => section.quizzes).flat()
+}
