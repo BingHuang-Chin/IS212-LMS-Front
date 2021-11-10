3132d48c59ec22be77b7bcc5730a322d239d749c
Bing Huang <bing2609@gmail.com>
2021-11-11 02:21:46 +0800

    js: self-enrolment: added display of courses
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- assets/js/self-enrolment.js
+++ assets/js/self-enrolment.js
@@ -17,10 +17,6 @@ async function getCourseList () {
       query: `
         query {
           course {
-            badge {
-              image
-            }
-            
             id
             title
             description
@@ -44,5 +40,30 @@ async function getCourseList () {
 }
 
 function renderCourseList (courses) {
-  console.log(courses)
+  courses.forEach(course => {
+    let { id, title, description, enrolment_end_date, enrolment_start_date } = course
+
+    enrolment_start_date = luxon.DateTime.fromISO(enrolment_start_date).toISODate()
+    enrolment_end_date = luxon.DateTime.fromISO(enrolment_end_date).toISODate()
+
+    $("#course-list").first().append(`
+      <div class="col p-2">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p>${description}</p>
+          </div>
+
+          <footer class="fs-6 text-secondary text-end px-3 pb-2">
+            <button class="d-block ms-auto btn btn-primary" onclick="onEnrolCourse(${id})">Enrol</button>
+            <span>Enrolment: ${enrolment_start_date} - ${enrolment_end_date}</span>
+          </footer>
+        </div>
+      </div>
+    `)
+  })
+}
+
+function onEnrolCourse(courseId) {
+  
 }
