94b80ef0a304c9bb50309d914a629d9a73671b96
Bing Huang <bing2609@gmail.com>
2021-11-11 00:55:54 +0800

    removed python tdd codes
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- assets/py-TDD/selfEnrolment.py
+++
@@ -1,6 +0,0 @@
-class self_enrolment:
-    __success = True
-    __userID = None
-
-    def enrol(self):
-        return (self.__success)
--- assets/py-TDD/test_enrolment.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from selfEnrolment import self_enrolment
-
-class testEnrolment(unittest.Testcase):
-    def test_enrolment(self):
-        self.assertEqual(success(), True)
